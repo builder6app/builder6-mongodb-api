ae83c963443759fd3c8defb1039c082e2189e9cd
Jack Zhuang <9340100@qq.com>
2024-12-13 22:30:45 +0800

    microsoft 365, get Site

--- src/microsoft365/microsoft365.controller.ts
+++ src/microsoft365/microsoft365.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Param } from '@nestjs/common';
 import { default as axios } from 'axios';
 import { Microsoft365Service } from './microsoft365.service';
 
@@ -42,6 +42,28 @@ export class Microsoft365Controller {
 
       return driveResponse.data;
     } catch (error) {
+      const message = error.response ? error.response.data : error.message;
+      throw new HttpException(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Get('sites/:siteId')
+  async getSite(
+    @Param('siteId') siteId : string = 'd4d4f221-7d18-4ba8-9800-7bbcd77263af'
+  ) {
+
+    try {
+
+      const accessToken = await this.microsoft365Service.getAccessToken();
+
+      const respose = await axios.get(
+        `https://graph.microsoft.com/v1.0/sites/${siteId}/drive`,
+        { headers: { Authorization: `Bearer ${accessToken}` } }
+      );
+
+      return respose.data;
+    } catch (error) {
+      console.log(error.response);
       const message = error.response ? error.response.data : error.message;
       throw new HttpException(message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
