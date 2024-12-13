093d2d10bb93a7c9d920400f7c6e3dfda6fe0f08
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2024-12-13 12:07:29 +0800

    odic: 提升安全性

--- src/oidc/oidc.controller.ts
+++ src/oidc/oidc.controller.ts
@@ -12,6 +12,7 @@ export class OidcController {
   @Get(':providerId/login')
   async login(
     @Param('providerId') providerId: string,
+    @Req() req,
     @Res() res
   ) {
     const provider = await this.oidcService.getProviderFromDB(providerId);
@@ -26,22 +27,30 @@ export class OidcController {
 
     const redirectTo = client.authorizationUrl({
       scope: provider.scope,
-      state: 'some_state_value',
+      state: provider.state,
+      nonce: provider.nonce,
       // resource: 'https://my.api.example.com/resource/32178',
       code_challenge: provider.code_challenge,
       code_challenge_method: provider.code_challenge_method,
     });    
     
+    req.session[`oidc_${providerId}_state`] = provider.state;
+    req.session[`oidc_${providerId}_code_verifier`] = provider.code_verifier;
+    req.session[`oidc_${providerId}_nonce`] = provider.nonce;
+
     return res.redirect(redirectTo);
   }
 
   @Get(':providerId/callback')
   async callback(
     @Param('providerId') providerId: string,
-    @Query() query,
     @Req() req,
     @Res() res
   ) {
+    const storedState = req.session[`oidc_${providerId}_state`];
+    const storedVerifier = req.session[`oidc_${providerId}_code_verifier`];
+    const storedNonce = req.session[`oidc_${providerId}_nonce`];
+
     const provider = await this.oidcService.getProviderFromDB(providerId);
     const issuer = await Issuer.discover(provider.issuer);
 
@@ -54,10 +63,19 @@ export class OidcController {
 
     const params = client.callbackParams(req);
 
-    const tokenSet = await client.callback(provider.redirect_uri, params, { code_verifier: provider.code_verifier });
+    const tokenSet = await client.callback(provider.redirect_uri, params, 
+      { 
+        state: storedState, 
+        code_verifier: storedVerifier, 
+        nonce: storedNonce 
+      });
     console.log('received and validated tokens %j', tokenSet);
     console.log('validated ID Token claims %j', tokenSet.claims());
     
+    delete req.session[`oidc_${providerId}_state`];
+    delete req.session[`oidc_${providerId}_code_verifier`];
+    delete req.session[`oidc_${providerId}_nonce`];
+
     const userInfo = await client.userinfo(tokenSet);
     console.log('userinfo %j', userInfo);
 
--- src/oidc/oidc.service.ts
+++ src/oidc/oidc.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { generators } from 'openid-client';
 
-const code_challenge_method = 'S256';
-const code_verifier = generators.codeVerifier();
 
 @Injectable()
 export class OidcService {
 
   async getProviderFromDB(providerId: string) {
 
+    const code_challenge_method = 'S256';
+    const code_verifier = generators.codeVerifier();
     const code_challenge = generators.codeChallenge(code_verifier);
+    const state = generators.random();
+    const nonce = generators.random(); // 生成 nonce
 
     // 从数据库获取 OIDC Provider 配置
     return {
@@ -19,6 +21,8 @@ export class OidcService {
       client_secret: process.env.STEEDOS_IDENTITY_OIDC_CLIENT_SECRET,
       redirect_uri: `${process.env.ROOT_URL}/api/v6/oidc/${providerId}/callback`,
       scope: process.env.STEEDOS_IDENTITY_OIDC_CLIENT_SCOPE || 'openid email profile',
+      state,
+      nonce,
       code_challenge_method,
       code_verifier,
       code_challenge,
